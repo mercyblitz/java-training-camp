e4bb23f1d1a517967c82b235d8f2e74c255a443b
Mercy Ma <mercyblitz@gmail.com>
2023-05-05 18:52:34 +0800

    Add RPC Project

---
+++ stage-2/src/middleware-projects/rpc-project/src/main/java/com/acme/middleware/rpc/serializer/DefaultSerializer.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.acme.middleware.rpc.serializer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * 默认 {@link Serializer}
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since 1.0.0
+ */
+public class DefaultSerializer implements Serializer {
+
+    @Override
+    public byte[] serialize(Object source) throws IOException {
+        byte[] bytes = null;
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream)
+        ) {
+            // Key -> byte[]
+            objectOutputStream.writeObject(source);
+            bytes = outputStream.toByteArray();
+        }
+        return bytes;
+    }
+
+    @Override
+    public Object deserialize(byte[] bytes, Class<?> targetClass) throws IOException {
+        if (bytes == null) {
+            return null;
+        }
+        Object value = null;
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
+             ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)
+        ) {
+            // byte[] -> Value
+            value = objectInputStream.readObject();
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
+        return value;
+    }
+}
